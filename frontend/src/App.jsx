57971a75568a4b2a761be6e6180a66122a3e0558
Kage Saluone <ksaluone17@gmail.com>
2025-08-17 22:31:29 -0600

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router'
 import Home from './Components/Home/Home'
+import Signup from './Components/Signup/Signup'
+import Login from './Components/Login/Login'
+import Profile from './Components/Profile/Profile'
 
 function App() {
     return (
@@ -10,6 +13,18 @@ function App() {
                 <Route 
                     path='/'
                     element={<Home />}
+                />
+                <Route 
+                    path='/signup'
+                    element={<Signup />}
+                />
+                <Route 
+                    path='/login'
+                    element={<Login />}
+                />
+                <Route 
+                    path='/profie'
+                    element={<Profile />}
                 />
             </Routes>
         </Router>
