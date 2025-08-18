184172c13666a16adeb6bd2a33231dfa6a1f7372
Kage Saluone <ksaluone17@gmail.com>
2025-08-17 22:39:50 -0600

    Created basic form with styling for signup

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router'
 
 function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    
     const navigate = useNavigate()
 
     return (
@@ -29,7 +35,36 @@ function Signup() {
                 </nav>
             </header>
             <div className='bg-zinc-100 flex flex-col w-full min-h-screen items-center p-8'>
-                <h2 className='text-3xl'>Sign Up</h2>
+                <h2 className='text-3xl mb-6'>Sign Up</h2>
+                <form action=""
+                    className='flex flex-wrap gap-2 bg-zinc-600 rounded-lg p-8'
+                >
+                    <input
+                        type="text"
+                        className='border-2 rounded-md px-3 py-1 border-zinc-9 bg-white'
+                        placeholder='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        className='border-2 rounded-md px-3 py-1 border-zinc-9 bg-white'
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        className='border-2 rounded-md px-3 py-1 border-zinc-900 bg-white'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button 
+                        type="submit"
+                        className='bg-green-500 text-sm border-3 border-white text-white px-3 py-1 rounded-lg cursor-pointer'
+                    >Sign Up</button>
+                </form>
             </div>
         </div>
     )
