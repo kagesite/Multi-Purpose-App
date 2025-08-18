4e64538b47d5a2b2af8e06cd0729a9e04653700c
Kage Saluone <ksaluone17@gmail.com>
2025-08-17 22:18:58 -0600

    Set up basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router'
+import Home from './Components/Home/Home'
 
 function App() {
-  return (
-    <div>
-        test
-    </div>
+    return (
+        <Router>
+            <Routes>
+                <Route 
+                    path='/'
+                    element={<Home />}
+                />
+            </Routes>
+        </Router>
   )
 }
 
