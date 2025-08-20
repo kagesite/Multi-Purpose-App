37ceb86d2990145555834988575225b1aba117a5
Kage Saluone <ksaluone17@gmail.com>
2025-08-19 21:19:41 -0600

    Created basic login api. It properly checks for the user and makes sure the passwords match.

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,18 @@ app.use(cors());
 
 const users = []
 
+const JWT_SECRET = "this_is_my_jwt_secret"
 
 // SIGNUP API
 app.post("/api/signup", async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        return res.status(400).json({ error: "Missing Information"});
+        return res.status(400).json({ error: "Missing Information" });
     }
-    
+
     const existingUser = users.find(user => user.email === email)
     if (existingUser) {
-        return res.status(401).json({ error: "User with email already exists!"});
+        return res.status(401).json({ error: "User with email already exists!" });
     }
 
     const saltRounds = 10;
@@ -34,20 +35,56 @@ app.post("/api/signup", async (req, res) => {
         email,
         password: hashedPassword
     }
-    
+
     users.push(newUser);
-    
+
     return res.status(200).json(
-        { 
-            message: "Sign Up Successful!", 
-            newUser: { 
+        {
+            message: "Sign Up Successful!",
+            newUser: {
                 id: newUser.id,
                 name: newUser.name,
                 email: newUser.email,
             }
         }
     )
+});
+
+
+// LOGIN API
+app.post("/api/login", async (req, res) => {
+    console.log("Reached Login")
+
+    try {
+        const { email, password } = req.body;
+        console.log(`Info: ${email}, ${password}`)
+        if (!email || !password) {
+            return res.status(400).json({ error: "Missing login information" });
+        }
+
+        const loginUser = users.find(user => user.email === email);
+        if (!loginUser) {
+            return res.status(401).json({ error: "No account found!" })
+        }
+
+        const isValid = await bcrypt.compare(password, loginUser.password);
+        if (!isValid) {
+            return res.status(500).json({ error: "Invalid password." });
+        }
+
+        return res.status(200).json({
+            message: "Login successful!",
+            loginUser
+        })
+    } catch (error) {
+        console.log(`Login Error: ${error}`);
+        return res.status(400).json({ error: `Login Error: ${error}` })
+    }
+
+
 })
+
+
 
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
