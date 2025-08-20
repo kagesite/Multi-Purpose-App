12d65c798e49b1c786fa1025c5a33ef25f4a72e4
Kage Saluone <ksaluone17@gmail.com>
2025-08-19 20:17:01 -0600

    Created reusable HeaderNav component for cleaner HTML

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,32 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import HeaderNav from '../../Components/HeaderNav';
 
 function Home() {
     const navigate = useNavigate();
     
     return (
         <div className='h-full'>
-            <header className='flex justify-between items-center bg-zinc-900 px-8 py-4'>
-                <h1 className='text-3xl text-white cursor-pointer'
-                    onClick={() => navigate("/")}
-                >KageApp</h1>
-                <nav className=''>
-                    <ul className='flex gap-6'>
-                        <li
-                            className='bg-green-500 text-sm border-3 border-white text-white px-3 py-1 rounded-lg cursor-pointer'
-                            onClick={() => navigate("/signup")}
-                        >
-                            Create Account
-                        </li>
-                        <li
-                            className='bg-amber-500 text-sm border-3 border-white text-white px-3 py-1 rounded-lg cursor-pointer'
-                            onClick={() => navigate("/login")}
-                        >
-                            Login
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+            <HeaderNav />
             <div className='bg-zinc-100 flex flex-col w-full min-h-screen items-center p-8'>
                 <div className='bg-zinc-200 p-8 w-lg rounded-lg border-2 mt-8'>
                     <h3 className='text-2xl mb-2 font-bold'>Welcome to Kage's Multi Purpose App</h3>
--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router'
+import HeaderNav from '../../Components/HeaderNav';
 
 function Login() {
     const [email, setEmail] = useState("");
@@ -17,28 +18,7 @@ function Login() {
     return (
         <div>
             <div>
-                <header className='flex justify-between items-center bg-zinc-900 px-8 py-4'>
-                    <h1 className='text-3xl text-white cursor-pointer'
-                        onClick={() => navigate("/")}
-                    >KageApp</h1>
-
-                    <nav className=''>
-                        <ul className='flex gap-6'>
-                            <li
-                                className='bg-green-500 text-sm border-3 border-white text-white px-3 py-1 rounded-lg cursor-pointer'
-                                onClick={() => navigate("/signup")}
-                            >
-                                Create Account
-                            </li>
-                            <li
-                                className='bg-amber-500 text-sm border-3 border-white text-white px-3 py-1 rounded-lg cursor-pointer'
-                                onClick={() => navigate("/login")}
-                            >
-                                Login
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
+            <HeaderNav />
                 <div className='bg-zinc-100 flex flex-col w-full min-h-screen items-center p-8'>
                     <h2 className='text-3xl mb-6'>Login</h2>
                     <form onSubmit={handleSubmit}
