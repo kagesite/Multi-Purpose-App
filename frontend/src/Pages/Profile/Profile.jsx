290a2d0866b30b06c9eb8787718fbbddb6341cdb
Kage Saluone <ksaluone17@gmail.com>
2025-08-20 20:47:29 -0600

    created a useEffect function to fetch the users data to display on the page

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -1,12 +1,62 @@
 import React from 'react'
 import HeaderNav from '../../Components/HeaderNav'
+import { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function Profile({ setIsLoggedIn }) {
+    const [user, setUser] = useState({});
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(true);
 
-    
+    useEffect(() => {
+        const getUserInfo = async () => {
+            setLoading(true);
+            try {
+                const token = localStorage.getItem("token");
+                if (!token) {
+                    setMessage("No token found");
+                    return
+                }
+
+                const url = "http://localhost:5000/api/auth/userInfo";
+                const response = await axios.get(url, {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+
+                setUser(response.data.userInfo);
+                setMessage("")
+            } catch (error) {
+                if (error.response) {
+                    setMessage(error.response.data.error)
+                } else {
+                    setMessage("Network error occurred");
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getUserInfo();
+    }, [])
+
+
     return (
         <div>
-            <HeaderNav setIsLoggedIn={setIsLoggedIn}/>
+            <HeaderNav setIsLoggedIn={setIsLoggedIn} />
+            <div>
+                {loading && <p>Loading...</p>}
+                {message && <p>{message}</p>}
+                {user && (
+                    <div>
+                        <h2>Welcome, {user.name}</h2>
+                        <p>Email: {user.email}</p>
+                        <p>ID: {user.id}</p>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
